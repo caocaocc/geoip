1d899e28e68a4388d495b6c0a4658004b666db6e
caocaocc <129647241+caocaocc@users.noreply.github.com>
2024-12-29 00:19:37 +0800

    Update release-branch.sh

--- .github/release-branch.sh
+++ .github/release-branch.sh
@@ -7,7 +7,7 @@ cd release
 git init
 git config --local user.email "github-action@users.noreply.github.com"
 git config --local user.name "GitHub Action"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/SagerNet/sing-geoip.git
+git remote add origin https://github-action:$GITHUB_TOKEN@github.com/caocaocc/geoip.git
 git branch -M release
 cp ../*.db ../*.sha256sum .
 git add .
